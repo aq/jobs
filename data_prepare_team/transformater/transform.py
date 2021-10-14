aeb4723d738d13615eded3548313a5e33fe2d855
Antoine Qu'hen <antoinequhen@gmail.com>
2021-10-15 00:04:53 +0200

    Allow direct connection to S3

--- data_prepare_team/transformater/transform.py
+++ data_prepare_team/transformater/transform.py
@@ -4,17 +4,19 @@ import click
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.types import StructType, StringType, ShortType
+from pyspark.conf import SparkConf
 
 class Transform():
 
-    def __init__(self, source_path, target_path, partitions_count, spark=None):
+    def __init__(self, source_path, target_path, partitions_count, aws_credentials, spark=None):
         self.source_path = source_path
         assert self.source_path, "source_path is required"
         self.target_path = target_path
         assert self.target_path, "target_path is required"
         self.partitions_count = int(partitions_count)
         assert self.partitions_count > 0, "partitions_count should be bigger than 0."
-        self.spark = spark or SparkSession.builder.getOrCreate()
+        conf = self.spark_conf(aws_credentials)
+        self.spark = spark or SparkSession.builder.config(conf=conf).getOrCreate()
 
     def run(self):
         source = self.load()
@@ -48,19 +50,33 @@ class Transform():
         if os.path.isdir(path):
             shutil.rmtree(path)
         dataframe.\
-            write.\
             repartition(self.partitions_count).\
+            write.\
             mode('overwrite').\
             option("compression", "gzip").\
             parquet(path)
+
+    @staticmethod
+    def spark_conf(aws_credentials):
+        return SparkConf().\
+            set("spark.hadoop.fs.s3a.path.style.access", True).\
+            set("spark.hadoop.fs.s3a.access.key", aws_credentials["aws_aki"]).\
+            set("spark.hadoop.fs.s3a.secret.key", aws_credentials["aws_sak"]).\
+            set("spark.hadoop.fs.s3a.endpoint", "s3-eu-west-1.amazonaws.com").\
+            set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem").\
+            set("com.amazonaws.services.s3.enableV4", True).\
+            set("spark.driver.extraJavaOptions", "-Dcom.amazonaws.services.s3.enableV4=true")
 
 @click.command()
 @click.option("-i", "--source_path", default="product_catalog.csv", help="Path where the source file is stored.")
 @click.option("-o", "--target_path", default="transform_output", help="Path where the results will be stored.")
 @click.option("-n", "--partitions_count", default="1", help="Number of partitions of the results.")
-def main(source_path, target_path, partitions_count):
+@click.option("--aws_aki", help="AWS access key")
+@click.option("--aws_sak", help="AWS secret")
+def main(source_path, target_path, partitions_count, aws_aki, aws_sak):
     """Entry point to run this Transform job."""
-    Transform(source_path, target_path, partitions_count).run()
+    aws_credentials = dict(aws_aki = aws_aki, aws_sak = aws_sak)
+    Transform(source_path, target_path, partitions_count, aws_credentials).run()
 
 if __name__ == '__main__':
     main()
